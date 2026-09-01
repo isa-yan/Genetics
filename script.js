e0b9b76afe2744be3eb8d3e92164247dd339f7dd
Isa Yan <isabel.yyj.yan@gmail.com>
2026-09-01 01:47:21 -0400

    basic logic ig

--- script.js
+++ script.js
@@ -11,8 +11,8 @@ let calc2 = document.getElementById('calc-two')
 let calc3 = document.getElementById('calc-three')
 let calc4 = document.getElementById('calc-four')
 
-let pheno1 = document.getElementById('pheno1')
-let pheno2 = document.getElementById('pheno2')
+let pheno_dom = document.getElementById('pheno_dominant')
+let pheno_rec = document.getElementById('pheno_recessive')
 
 let pure1 = document.getElementById('pure1')
 let half1 = document.getElementById('half1')
@@ -43,6 +43,40 @@ pure2.addEventListener("click",()=>{
 half2.addEventListener("click",()=>{
     half2.classList.add("clicked")
     pure2.classList.remove("clicked")
+
+})
+
+let timer;
+const donetyping = 500;
+
+let option=document.createElement("option")
+let option2=document.createElement("option")
+let option3=document.createElement("option")
+let option4=document.createElement("option")
+
+pheno_dom.addEventListener('input',()=>{
+    clearTimeout(donetyping)
+    timer = setTimeout(()=>{
+        parent_pheno1.append(option)
+        parent_pheno1[0].value=pheno_dom.value
+        parent_pheno1[0].textContent=pheno_dom.value
+        parent_pheno2.append(option2)
+        parent_pheno2[0].value=pheno_dom.value
+        parent_pheno2[0].textContent=pheno_dom.value
+    },donetyping)
+
+})
+
+pheno_rec.addEventListener('input',()=>{
+    clearTimeout(donetyping)
+    timer = setTimeout(()=>{
+        parent_pheno1.append(option3)
+        parent_pheno1[1].value=pheno_rec.value
+        parent_pheno1[1].textContent=pheno_rec.value
+        parent_pheno2.append(option4)
+        parent_pheno2[1].value=pheno_rec.value
+        parent_pheno2[1].textContent=pheno_rec.value
+    },donetyping)
 
 })
 
